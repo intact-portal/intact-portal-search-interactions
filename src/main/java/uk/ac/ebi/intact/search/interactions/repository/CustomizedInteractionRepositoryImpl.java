e9bcda902be19d72ef7ac4e698fcd2323f9465c6
ntoro <ntoro@ebi.ac.uk>
2019-05-29 15:48:17 +0100

    Updates javadoc

--- src/main/java/uk/ac/ebi/intact/search/interactions/repository/CustomizedInteractionRepositoryImpl.java
+++ src/main/java/uk/ac/ebi/intact/search/interactions/repository/CustomizedInteractionRepositoryImpl.java
@@ -41,19 +41,18 @@ public class CustomizedInteractionRepositoryImpl implements CustomizedInteractio
     }
 
     /**
-     * @param query
-     * @param detectionMethodFilter (Optional)
-     * @param interactionTypeFilter (Optional)
-     * @param hostOrganismFilter    (Optional)
-     * @param isNegativeFilter      (Optional)
-     * @param minMiScore
-     * @param maxMiScore
-     * @param species               (Optional)
-     * @param interSpecies :         if true it expects two species and returned interactions should be between the given two species
-     *                              if false interactions should atleast have one of the given species
-     * @param sort
-     * @param pageable
-     * @return
+     * @param query input used to retrieve the interaction
+     * @param detectionMethodFilter (Optional) filter interactions by interaction detection method
+     * @param interactionTypeFilter (Optional) filter interactions by interaction type
+     * @param hostOrganismFilter (Optional) filter interactions by host organism
+     * @param isNegativeFilter (Optional) filter interactions that are negative if true
+     * @param minMiScore minimun value of mi-score for the interaction
+     * @param maxMiScore minimun value of mi-score for the interaction
+     * @param species (Optional) interactor species of the interaction
+     * @param interSpecies boolean to restrict the result ot the same or different interactor species
+     * @param sort field to define the sort of the results
+     * @param pageable  page number and size of the request
+     * @return the interaction data matching all the criteria
      */
     @Override
     public FacetPage<SearchInteraction> findInteractionWithFacet(String query, Set<String> detectionMethodFilter,
@@ -112,19 +111,18 @@ public class CustomizedInteractionRepositoryImpl implements CustomizedInteractio
     }
 
     /**
-     * @param query
-     * @param detectionMethodFilter (Optional)
-     * @param interactionTypeFilter (Optional)
-     * @param hostOrganismFilter    (Optional)
-     * @param isNegativeFilter      (Optional)
-     * @param minMiScore
-     * @param maxMiScore
-     * @param species               (Optional)
-     * @param interSpecies:         if true it expects two species and returned interactions should be between the given two species
-     *                              if false interactions should atleast have one of the given species
-     * @param sort
-     * @param pageable
-     * @return
+     * @param query input used to retrieve the interaction
+     * @param detectionMethodFilter (Optional) filter interactions by interaction detection method
+     * @param interactionTypeFilter (Optional) filter interactions by interaction type
+     * @param hostOrganismFilter (Optional) filter interactions by host organism
+     * @param isNegativeFilter (Optional) filter interactions that are negative if true
+     * @param minMiScore minimun value of mi-score for the interaction
+     * @param maxMiScore minimun value of mi-score for the interaction
+     * @param species (Optional) interactor species of the interaction
+     * @param interSpecies boolean to restrict the result ot the same or different interactor species
+     * @param sort field to define the sort of the results
+     * @param pageable  page number and size of the request
+     * @return the interaction page matching all the criteria for graphical representation
      */
     @Override
     public Page<SearchInteraction> findInteractionForGraphJson(String query, Set<String> detectionMethodFilter,
@@ -171,6 +169,19 @@ public class CustomizedInteractionRepositoryImpl implements CustomizedInteractio
         return solrOperations.queryForPage(INTERACTIONS, search, SearchInteraction.class);
     }
 
+    /**
+     * @param query input used to retrieve the interaction
+     * @param interactorAc interactor accession e.g. EBI-XXXXXX
+     * @param detectionMethodFilter (Optional) filter interactions by interaction detection method
+     * @param interactionTypeFilter (Optional) filter interactions by interaction type
+     * @param hostOrganismFilter (Optional) filter interactions by host organism
+     * @param isNegativeFilter (Optional) filter interactions that are negative if true
+     * @param minMiScore minimun value of mi-score for the interaction
+     * @param maxMiScore minimun value of mi-score for the interaction
+     * @param species (Optional) interactor species of the interaction
+     * @param interSpecies boolean to restrict the result ot the same or different interactor species
+     * @return the number of interactions matching all the criteria
+     */
     @Override
     public long countInteractionResult(String query, String interactorAc, Set<String> detectionMethodFilter,
                                        Set<String> interactionTypeFilter, Set<String> hostOrganismFilter,
