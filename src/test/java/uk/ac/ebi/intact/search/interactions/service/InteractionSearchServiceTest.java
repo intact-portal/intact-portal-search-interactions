423b9cf2dd32b06819c573286795a18681dc633e
ntoro <ntoro@ebi.ac.uk>
2019-05-29 15:23:22 +0100

    NIV-287: Adapts test to new configuration

--- src/test/java/uk/ac/ebi/intact/search/interactions/service/InteractionSearchServiceTest.java
+++ src/test/java/uk/ac/ebi/intact/search/interactions/service/InteractionSearchServiceTest.java
@@ -1,15 +1,14 @@
 package uk.ac.ebi.intact.search.interactions.service;
 
+import org.junit.After;
 import org.junit.Before;
-import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.test.context.junit4.SpringRunner;
+import uk.ac.ebi.intact.search.interactions.controller.SearchInteractionResult;
 import uk.ac.ebi.intact.search.interactions.model.SearchInteraction;
-import uk.ac.ebi.intact.search.interactions.model.SearchInteractionResult;
-import uk.ac.ebi.intact.search.interactions.service.util.RequiresSolrServer;
 import uk.ac.ebi.intact.search.interactions.service.util.TestUtil;
 
 import javax.annotation.Resource;
@@ -19,18 +18,15 @@ import java.util.List;
 import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static uk.ac.ebi.intact.search.interactions.model.SearchInteractionFields.*;
 
 /**
  * @author Elisabet Barrera
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SearchInteractionIndexServiceTest {
-
-    private SearchInteraction searchInteraction1;
-    private SearchInteraction searchInteraction2;
-
-    private Collection<SearchInteraction> searchInteractions;
+public class InteractionSearchServiceTest {
 
     @Resource
     private InteractionIndexService interactionIndexService;
@@ -38,87 +34,98 @@ public class SearchInteractionIndexServiceTest {
     @Resource
     private InteractionSearchService interactionSearchService;
 
-    public static
-    @ClassRule
-    RequiresSolrServer requiresRunningServer = RequiresSolrServer.onLocalhost();
-
     /**
      * Before any tests run, this cleans the solr index and creates a new index with stored searchInteractions in an xml
-     * @throws Exception
      */
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
 
         //Delete all documents from solr core
         interactionIndexService.deleteAll();
         /*Interactions are instantiated from saved searchInteractions in an xml as instantiating it one by one in the code is cumbersome
         * For ref. The Interactions.xml can be created with a method saveInteractioninDisc in CommonUtility in intact-portal-indexer*/
-        searchInteractions = TestUtil.getInteractionObjFromXml("./src/test/resources/Interactions.xml");
+        Collection<SearchInteraction> searchInteractions = TestUtil.getInteractionObjFromXml("./src/test/resources/Interactions.xml");
         interactionIndexService.save(searchInteractions);
-        assertEquals(searchInteractions.size(), 10);
+        assertEquals(interactionSearchService.countTotal(), searchInteractions.size());
     }
 
-    @Test
+    @After
+    public void tearDown() {
+        interactionIndexService.deleteAll();
+    }
+
     /**
      * Behaviour If the User types "Author name" in search box
      */
+    @Test
     public void findByAuthor() {
         Page<SearchInteraction> interactionOp = interactionSearchService.findInteractions("Shorter J.");
         assertEquals(interactionOp.getNumberOfElements(), 4);
     }
 
-    @Test
     /**
      * Behaviour If the User types "Species Name" in search box
      */
+    @Test
     public void findBySpecies() {
         Page<SearchInteraction> interactionOp = interactionSearchService.findInteractions("Rattus norvegicus (Rat)");
         assertEquals(interactionOp.getNumberOfElements(), 4);
     }
 
-    @Test
     /**
      * Behaviour If the User types "SearchInteraction Type" in search box
      */
+    @Test
     public void findByInteractionType() {
         Page<SearchInteraction> interactionOp = interactionSearchService.findInteractions("physical association");
         assertEquals(interactionOp.getNumberOfElements(), 10);
     }
 
-    @Test
     /**
      * Behaviour If the User types "SearchInteraction Detection Method" in search box
      */
+    @Test
     public void findByIntDetMethod() {
         Page<SearchInteraction> interactionOp = interactionSearchService.findInteractions("molecular sieving");
         assertEquals(interactionOp.getNumberOfElements(), 1);
     }
 
-    @Test
     /**
      * Behaviour If the User types "Host Organism" in search box
      */
+    @Test
     public void findByHostOrganism() {
         Page<SearchInteraction> interactionOp = interactionSearchService.findInteractions("\"In vitro\"");
         assertEquals(interactionOp.getNumberOfElements(), 6);
     }
 
-    @Test
     /**
      * Expected Facet Results If the User types "physical association" and Hit Search Button
      */
+    @Test
     public void facetTest() {
-        SearchInteractionResult interactionOp = interactionSearchService.findInteractionWithFacet("physical association", null, null, null, false, 0, 1, null, false, 0, 10);
+        SearchInteractionResult interactionOp = interactionSearchService.findInteractionWithFacet(
+                "physical association",
+                null,
+                null,
+                null,
+                false,
+                0,
+                1,
+                null,
+                false,
+                0,
+                10);
         Set<String> facetFields = interactionOp.getFacetResultPage().keySet();
-        assertEquals(true, facetFields.contains("interaction_negative"));
-        assertEquals(true, facetFields.contains("intact_miscore"));
-        assertEquals(true, facetFields.contains("interaction_detection_method_str"));
-        assertEquals(true, facetFields.contains("interaction_type_str"));
-        assertEquals(true, facetFields.contains("host_organism_str"));
-        assertEquals(true, facetFields.contains("speciesA_B_str"));
+        assertTrue(facetFields.contains(INTERACTION_NEGATIVE));
+        assertTrue(facetFields.contains(INTACT_MISCORE));
+        assertTrue(facetFields.contains(INTERACTION_DETECTION_METHOD_STR));
+        assertTrue(facetFields.contains(INTERACTION_TYPE_STR));
+        assertTrue(facetFields.contains(HOST_ORGANISM_STR));
+        assertTrue(facetFields.contains(SPECIES_A_B_STR));
 
         for (String facetField : interactionOp.getFacetResultPage().keySet()) {
-            if (facetField.equals("interaction_negative")) {
+            if (facetField.equals(INTERACTION_NEGATIVE)) {
                 List<SearchInteractionResult.FacetCount> facetCounts = interactionOp.getFacetResultPage().get(facetField);
                 for (SearchInteractionResult.FacetCount facetCount : facetCounts) {
                     if (facetCount.getValue().equals("false")) {
@@ -127,7 +134,7 @@ public class SearchInteractionIndexServiceTest {
                 }
             }
 
-            if (facetField.equals("intact_miscore")) {
+            if (facetField.equals(INTACT_MISCORE)) {
                 List<SearchInteractionResult.FacetCount> facetCounts = interactionOp.getFacetResultPage().get(facetField);
                 for (SearchInteractionResult.FacetCount facetCount : facetCounts) {
                     if (facetCount.getValue().equals("0.39")) {
@@ -145,7 +152,7 @@ public class SearchInteractionIndexServiceTest {
                 }
             }
 
-            if (facetField.equals("interaction_detection_method_str")) {
+            if (facetField.equals(INTERACTION_DETECTION_METHOD_STR)) {
                 List<SearchInteractionResult.FacetCount> facetCounts = interactionOp.getFacetResultPage().get(facetField);
                 for (SearchInteractionResult.FacetCount facetCount : facetCounts) {
                     if (facetCount.getValue().equals("anti bait coip")) {
@@ -163,7 +170,7 @@ public class SearchInteractionIndexServiceTest {
                 }
             }
 
-            if (facetField.equals("interaction_type_str")) {
+            if (facetField.equals(INTERACTION_TYPE_STR)) {
                 List<SearchInteractionResult.FacetCount> facetCounts = interactionOp.getFacetResultPage().get(facetField);
                 for (SearchInteractionResult.FacetCount facetCount : facetCounts) {
                     if (facetCount.getValue().equals("physical association")) {
@@ -172,7 +179,7 @@ public class SearchInteractionIndexServiceTest {
                 }
             }
 
-            if (facetField.equals("host_organism_str")) {
+            if (facetField.equals(HOST_ORGANISM_STR)) {
                 List<SearchInteractionResult.FacetCount> facetCounts = interactionOp.getFacetResultPage().get(facetField);
                 for (SearchInteractionResult.FacetCount facetCount : facetCounts) {
                     if (facetCount.getValue().equals("In vitro")) {
@@ -184,7 +191,7 @@ public class SearchInteractionIndexServiceTest {
                 }
             }
 
-            if (facetField.equals("speciesA_B_str")) {
+            if (facetField.equals(SPECIES_A_B_STR)) {
                 List<SearchInteractionResult.FacetCount> facetCounts = interactionOp.getFacetResultPage().get(facetField);
                 for (SearchInteractionResult.FacetCount facetCount : facetCounts) {
                     if (facetCount.getValue().equals("Homo sapiens")) {
@@ -200,87 +207,87 @@ public class SearchInteractionIndexServiceTest {
         assertEquals(10, interactionOp.getNumberOfElements());
     }
 
-    @Test
     /**
      * Expected behaviour when filter elements are passed
      */
-    public void filterTest1() {
+    @Test
+    public void filterInterSpeciesFalseOneSpecies() {
 
         //filter1
-
-        Set<String> detectionMethod = new HashSet<String>();
+        Set<String> detectionMethod = new HashSet<>();
         detectionMethod.add("molecular sieving");
 
-        Set<String> interactiontype = new HashSet<String>();
-        interactiontype.add("physical association");
+        Set<String> interactionType = new HashSet<>();
+        interactionType.add("physical association");
 
-        Set<String> hostOrganism = new HashSet<String>();
+        Set<String> hostOrganism = new HashSet<>();
         hostOrganism.add("In vitro");
 
-        boolean isNegative = false;
-
         double minMiscore = 0;
-
         double maxMiscore = 0.6;
 
-        Set<String> species = new HashSet<String>();
+        Set<String> species = new HashSet<>();
         species.add("Homo sapiens");
 
-        boolean interSpecies = false;
-
         int page = 0;
-
         int size = 10;
 
-        SearchInteractionResult interactionOp = interactionSearchService.
-                findInteractionWithFacet("physical association", detectionMethod, interactiontype, hostOrganism, isNegative, minMiscore, maxMiscore, species, false, page, size);
+        SearchInteractionResult interactionOp = interactionSearchService.findInteractionWithFacet(
+                "physical association",
+                detectionMethod,
+                interactionType,
+                hostOrganism,
+                false,
+                minMiscore,
+                maxMiscore,
+                species,
+                false,
+                page,
+                size);
         assertEquals(1, interactionOp.getNumberOfElements());
 
     }
 
-    @Test
     /**
      * Expected behaviour when filter elements are passed
      */
-    public void filterTest2() {
+    @Test
+    public void filterInterSpeciesTrueTwoSpecies() {
 
         //filter2
-
-        Set<String> detectionMethod = new HashSet<String>();
+        Set<String> detectionMethod = new HashSet<>();
         detectionMethod.add("molecular sieving");
 
-        Set<String> interactiontype = new HashSet<String>();
-        interactiontype.add("physical association");
+        Set<String> interactionType = new HashSet<>();
+        interactionType.add("physical association");
 
-        Set<String> hostOrganism = new HashSet<String>();
+        Set<String> hostOrganism = new HashSet<>();
         hostOrganism.add("In vitro");
 
-        boolean isNegative = false;
-
         double minMiscore = 0;
-
         double maxMiscore = 0.6;
 
-        Set<String> species = new HashSet<String>();
+        Set<String> species = new HashSet<>();
         species.add("Homo sapiens");
         species.add("Rattus norvegicus (Rat)");
-
-        boolean interSpecies = true;
 
         int page = 0;
 
         int size = 10;
 
-        SearchInteractionResult interactionOp = interactionSearchService.
-                findInteractionWithFacet("physical association", detectionMethod, interactiontype, hostOrganism, isNegative, minMiscore, maxMiscore, species, interSpecies, page, size);
+        SearchInteractionResult interactionOp = interactionSearchService.findInteractionWithFacet(
+                "physical association",
+                detectionMethod,
+                interactionType,
+                hostOrganism,
+                false,
+                minMiscore,
+                maxMiscore,
+                species,
+                true,
+                page,
+                size);
         assertEquals(0, interactionOp.getNumberOfElements());
-
-    }
-
-    @Test
-    public void deleteCollection() {
-        // empty collection
-        interactionIndexService.deleteAll();
     }
 
 }
