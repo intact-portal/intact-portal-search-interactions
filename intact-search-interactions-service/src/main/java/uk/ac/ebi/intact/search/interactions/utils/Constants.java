19d4a70bf66529885e953c6490f1822ad085ac91
ntoro <ntoro@ebi.ac.uk>
2020-05-27 23:23:41 +0100

    Removes the Constants.java because it is not part of the service library. It is use only in the web service. For now it could be redundant to add it in each ws, but it helps to separate the responsibilities of the libraries in the long term maintenance.

--- intact-search-interactions-service/src/main/java/uk/ac/ebi/intact/search/interactions/utils/Constants.java
+++
@@ -1,8 +0,0 @@
-package uk.ac.ebi.intact.search.interactions.utils;
-
-/**
- * Created by anjali on 17/02/20.
- */
-public class Constants {
-    public static final String UPLOADED_BATCH_FILE_PREFIX = "file_";
-}
