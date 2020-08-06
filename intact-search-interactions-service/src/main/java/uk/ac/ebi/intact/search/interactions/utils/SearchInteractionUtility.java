ca9674a3c537f9bc8fe5e44e4d6770886691a213
anjali <anjali@ebi.ac.uk>
2020-08-06 14:37:23 +0100

    NIV-453: Ac search term will be searched in only in Ac fields and not in default field

--- intact-search-interactions-service/src/main/java/uk/ac/ebi/intact/search/interactions/utils/SearchInteractionUtility.java
+++ intact-search-interactions-service/src/main/java/uk/ac/ebi/intact/search/interactions/utils/SearchInteractionUtility.java
@@ -5,6 +5,8 @@ import org.springframework.data.solr.core.query.FilterQuery;
 import org.springframework.data.solr.core.query.SimpleFilterQuery;
 
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static uk.ac.ebi.intact.search.interactions.model.SearchInteractionFields.*;
 
@@ -21,7 +23,7 @@ public class SearchInteractionUtility {
 
         //We prepare the term to split by several characters
 
-        if(searchTerms != null && !searchTerms.isEmpty()) {
+        if (searchTerms != null && !searchTerms.isEmpty()) {
             words = Arrays.asList(searchTerms.split("[\\s,\\n]"));
 
             if (batchSearch) {
@@ -31,15 +33,21 @@ public class SearchInteractionUtility {
                 if (!searchTerms.trim().equals("*")) {
                     for (String word : words) {
                         if (userConditions == null) {
-                            userConditions = new Criteria(DEFAULT).is(word)
-                                    .or(AC_A_STR).is(word)
-                                    .or(AC_B_STR).is(word)
-                                    .or(AC_STR).is(word);
+                            if (isEBIAc(word)) {
+                                userConditions = new Criteria(AC_A_STR).is(word)
+                                        .or(AC_B_STR).is(word)
+                                        .or(AC_STR).is(word);
+                            } else {
+                                userConditions = new Criteria(DEFAULT).is(word);
+                            }
                         } else {
-                            userConditions = userConditions.or(DEFAULT).is(word)
-                                    .or(AC_A_STR).is(word)
-                                    .or(AC_B_STR).is(word)
-                                    .or(AC_STR).is(word);
+                            if (isEBIAc(word)) {
+                                userConditions = userConditions.or(AC_A_STR).is(word)
+                                        .or(AC_B_STR).is(word)
+                                        .or(AC_STR).is(word);
+                            } else {
+                                userConditions = userConditions.or(DEFAULT).is(word);
+                            }
                         }
                     }
                 }
@@ -164,5 +172,18 @@ public class SearchInteractionUtility {
 
             filterQueries.add(new SimpleFilterQuery(conditions));
         }
+    }
+
+    private boolean isEBIAc(String term) {
+        String pattern = "EBI-" + "[0-9]+";
+        Pattern r1 = Pattern.compile(pattern);
+
+        Matcher m = r1.matcher(term);
+        boolean matched = m.matches();
+
+        if (matched) {
+            return true;
+        }
+        return false;
     }
 }
