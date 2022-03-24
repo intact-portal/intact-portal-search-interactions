b780961633bd77c1d056467c2d0ea605c834516a
anjali <anjali@ebi.ac.uk>
2022-03-24 14:58:23 +0000

    NIV-730 : Advanced search - getters removed as not needed

--- intact-search-interactions-service/src/main/java/uk/ac/ebi/intact/search/interactions/model/AdvancedSearchInteraction.java
+++ intact-search-interactions-service/src/main/java/uk/ac/ebi/intact/search/interactions/model/AdvancedSearchInteraction.java
@@ -133,328 +133,164 @@ public class AdvancedSearchInteraction {
     @Field(GENE_NAME_B)
     private String asGeneNameB;
 
-    public Set<String> getAsIdA() {
-        return asIdA;
-    }
-
     public void setAsIdA(Set<String> asIdA) {
         this.asIdA = asIdA;
-    }
-
-    public Set<String> getAsIdB() {
-        return asIdB;
     }
 
     public void setAsIdB(Set<String> asIdB) {
         this.asIdB = asIdB;
     }
 
-    public Set<String> getAsAltidA() {
-        return asAltidA;
-    }
-
     public void setAsAltidA(Set<String> asAltidA) {
         this.asAltidA = asAltidA;
-    }
-
-    public Set<String> getAsAltidB() {
-        return asAltidB;
     }
 
     public void setAsAltidB(Set<String> asAltidB) {
         this.asAltidB = asAltidB;
     }
 
-    public Set<String> getAsAliasA() {
-        return asAliasA;
-    }
-
     public void setAsAliasA(Set<String> asAliasA) {
         this.asAliasA = asAliasA;
-    }
-
-    public Set<String> getAsAliasB() {
-        return asAliasB;
     }
 
     public void setAsAliasB(Set<String> asAliasB) {
         this.asAliasB = asAliasB;
     }
 
-    public Set<String> getAsPubId() {
-        return asPubId;
-    }
-
     public void setAsPubId(Set<String> asPubId) {
         this.asPubId = asPubId;
-    }
-
-    public Set<String> getAsInteractionXrefs() {
-        return asInteractionXrefs;
     }
 
     public void setAsInteractionXrefs(Set<String> asInteractionXrefs) {
         this.asInteractionXrefs = asInteractionXrefs;
     }
 
-    public Set<String> getAsTaxIdA() {
-        return asTaxIdA;
-    }
-
     public void setAsTaxIdA(Set<String> asTaxIdA) {
         this.asTaxIdA = asTaxIdA;
-    }
-
-    public Set<String> getAsTaxIdB() {
-        return asTaxIdB;
     }
 
     public void setAsTaxIdB(Set<String> asTaxIdB) {
         this.asTaxIdB = asTaxIdB;
     }
 
-    public Set<String> getAsPubAuthors() {
-        return asPubAuthors;
-    }
-
     public void setAsPubAuthors(Set<String> asPubAuthors) {
         this.asPubAuthors = asPubAuthors;
-    }
-
-    public Set<String> getAsPubFirstAuthor() {
-        return asPubFirstAuthor;
     }
 
     public void setAsPubFirstAuthor(Set<String> asPubFirstAuthor) {
         this.asPubFirstAuthor = asPubFirstAuthor;
     }
 
-    public Integer getAsPubYear() {
-        return asPubYear;
-    }
-
     public void setAsPubYear(Integer asPubYear) {
         this.asPubYear = asPubYear;
-    }
-
-    public Set<String> getAsType() {
-        return asType;
     }
 
     public void setAsType(Set<String> asType) {
         this.asType = asType;
     }
 
-    public Set<String> getAsIDetectionMethod() {
-        return asIDetectionMethod;
-    }
-
     public void setAsIDetectionMethod(Set<String> asIDetectionMethod) {
         this.asIDetectionMethod = asIDetectionMethod;
-    }
-
-    public Set<String> getAsHostOrganism() {
-        return asHostOrganism;
     }
 
     public void setAsHostOrganism(Set<String> asHostOrganism) {
         this.asHostOrganism = asHostOrganism;
     }
 
-    public Set<String> getAsBioRoleA() {
-        return asBioRoleA;
-    }
-
     public void setAsBioRoleA(Set<String> asBioRoleA) {
         this.asBioRoleA = asBioRoleA;
-    }
-
-    public Set<String> getAsBioRoleB() {
-        return asBioRoleB;
     }
 
     public void setAsBioRoleB(Set<String> asBioRoleB) {
         this.asBioRoleB = asBioRoleB;
     }
 
-    public Set<String> getAsTypeA() {
-        return asTypeA;
-    }
-
     public void setAsTypeA(Set<String> asTypeA) {
         this.asTypeA = asTypeA;
-    }
-
-    public Set<String> getAsTypeB() {
-        return asTypeB;
     }
 
     public void setAsTypeB(Set<String> asTypeB) {
         this.asTypeB = asTypeB;
     }
 
-    public Set<String> getAsFeatureTypesA() {
-        return asFeatureTypesA;
-    }
-
     public void setAsFeatureTypesA(Set<String> asFeatureTypesA) {
         this.asFeatureTypesA = asFeatureTypesA;
-    }
-
-    public Set<String> getAsFeatureTypesB() {
-        return asFeatureTypesB;
     }
 
     public void setAsFeatureTypesB(Set<String> asFeatureTypesB) {
         this.asFeatureTypesB = asFeatureTypesB;
     }
 
-    public Set<String> getAsIdentificationMethodsA() {
-        return asIdentificationMethodsA;
-    }
-
     public void setAsIdentificationMethodsA(Set<String> asIdentificationMethodsA) {
         this.asIdentificationMethodsA = asIdentificationMethodsA;
-    }
-
-    public Set<String> getAsIdentificationMethodsB() {
-        return asIdentificationMethodsB;
     }
 
     public void setAsIdentificationMethodsB(Set<String> asIdentificationMethodsB) {
         this.asIdentificationMethodsB = asIdentificationMethodsB;
     }
 
-    public Set<String> getAsXrefsA() {
-        return asXrefsA;
-    }
-
     public void setAsXrefsA(Set<String> asXrefsA) {
         this.asXrefsA = asXrefsA;
-    }
-
-    public Set<String> getAsXrefsB() {
-        return asXrefsB;
     }
 
     public void setAsXrefsB(Set<String> asXrefsB) {
         this.asXrefsB = asXrefsB;
     }
 
-    public Set<String> getAsInteractionIds() {
-        return asInteractionIds;
-    }
-
     public void setAsInteractionIds(Set<String> asInteractionIds) {
         this.asInteractionIds = asInteractionIds;
-    }
-
-    public Set<String> getAsSource() {
-        return asSource;
     }
 
     public void setAsSource(Set<String> asSource) {
         this.asSource = asSource;
     }
 
-    public Set<String> getAsExpansionMethod() {
-        return asExpansionMethod;
-    }
-
     public void setAsExpansionMethod(Set<String> asExpansionMethod) {
         this.asExpansionMethod = asExpansionMethod;
-    }
-
-    public Integer getAsUpdationDate() {
-        return asUpdationDate;
     }
 
     public void setAsUpdationDate(Integer asUpdationDate) {
         this.asUpdationDate = asUpdationDate;
     }
 
-    public double getAsIntactMiscore() {
-        return asIntactMiscore;
+    public void setAsReleaseDate(Integer asReleaseDate) {
+        this.asReleaseDate = asReleaseDate;
     }
 
     public void setAsIntactMiscore(double asIntactMiscore) {
         this.asIntactMiscore = asIntactMiscore;
     }
 
-    public Integer getAsReleaseDate() {
-        return asReleaseDate;
-    }
-
-    public void setAsReleaseDate(Integer asReleaseDate) {
-        this.asReleaseDate = asReleaseDate;
-    }
-
-    public boolean isAsNegative() {
-        return asNegative;
-    }
-
     public void setAsNegative(boolean asNegative) {
         this.asNegative = asNegative;
-    }
-
-    public boolean isAsParam() {
-        return asParam;
     }
 
     public void setAsParam(boolean asParam) {
         this.asParam = asParam;
     }
 
-    public boolean isAsStoichiometry() {
-        return asStoichiometry;
-    }
-
     public void setAsStoichiometry(boolean asStoichiometry) {
         this.asStoichiometry = asStoichiometry;
-    }
-
-    public boolean isAsAffectedByMutation() {
-        return asAffectedByMutation;
     }
 
     public void setAsAffectedByMutation(boolean asAffectedByMutation) {
         this.asAffectedByMutation = asAffectedByMutation;
     }
 
-    public boolean isAsMutationA() {
-        return asMutationA;
-    }
-
     public void setAsMutationA(boolean asMutationA) {
         this.asMutationA = asMutationA;
-    }
-
-    public boolean isAsMutationB() {
-        return asMutationB;
     }
 
     public void setAsMutationB(boolean asMutationB) {
         this.asMutationB = asMutationB;
     }
 
-    public Set<String> getAsAnnotations() {
-        return asAnnotations;
-    }
-
     public void setAsAnnotations(Set<String> asAnnotations) {
         this.asAnnotations = asAnnotations;
     }
 
-    public String getAsGeneNameA() {
-        return asGeneNameA;
-    }
-
     public void setAsGeneNameA(String asGeneNameA) {
         this.asGeneNameA = asGeneNameA;
-    }
-
-    public String getAsGeneNameB() {
-        return asGeneNameB;
     }
 
     public void setAsGeneNameB(String asGeneNameB) {
