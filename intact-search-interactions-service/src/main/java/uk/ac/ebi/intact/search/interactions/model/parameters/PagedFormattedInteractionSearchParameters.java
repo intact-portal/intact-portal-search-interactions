dc3fed0b4366f29e845924349704cbe4032500d3
jmedinaebi <jmedina@ebi.ac.uk>
2025-06-02 11:22:58 +0100

    Fix copyParameters methods

--- intact-search-interactions-service/src/main/java/uk/ac/ebi/intact/search/interactions/model/parameters/PagedFormattedInteractionSearchParameters.java
+++ intact-search-interactions-service/src/main/java/uk/ac/ebi/intact/search/interactions/model/parameters/PagedFormattedInteractionSearchParameters.java
@@ -15,6 +15,30 @@ public class PagedFormattedInteractionSearchParameters extends PagedInteractionS
     protected InteractionExportFormat format = InteractionExportFormat.miJSON;
 
     public static PagedFormattedInteractionSearchParametersBuilder<?,?> copyParameters(PagedInteractionSearchParameters parameters) {
-        return (PagedFormattedInteractionSearchParametersBuilder<?,?>) parameters.toBuilder();
+        return PagedFormattedInteractionSearchParameters.builder()
+                .query(parameters.getQuery())
+                .batchSearch(parameters.isBatchSearch())
+                .advancedSearch(parameters.isAdvancedSearch())
+
+                .interactionDetectionMethodsFilter(parameters.getInteractionDetectionMethodsFilter())
+                .interactionTypesFilter(parameters.getInteractionTypesFilter())
+                .interactionHostOrganismsFilter(parameters.getInteractionHostOrganismsFilter())
+                .interactorTypesFilter(parameters.getInteractorTypesFilter())
+                .interactorSpeciesFilter(parameters.getInteractorSpeciesFilter())
+
+                .intraSpeciesFilter(parameters.isIntraSpeciesFilter())
+                .expansionFilter(parameters.isExpansionFilter())
+                .mutationFilter(parameters.isMutationFilter())
+                .negativeFilter(parameters.getNegativeFilter())
+
+                .maxMIScore(parameters.getMaxMIScore())
+                .minMIScore(parameters.getMinMIScore())
+
+                .binaryInteractionIds(parameters.getBinaryInteractionIds())
+                .interactorAcs(parameters.getInteractorAcs())
+
+                .page(parameters.getPage())
+                .pageSize(parameters.getPageSize())
+                .sort(parameters.getSort());
     }
 }
